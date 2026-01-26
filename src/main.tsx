f4c0a98ea5ebb086b27025c468bbb4929dcc3813
YuanSue <guardianwings1996@gmail.com>
2026-01-26 15:34:12 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+/*解決子節點404問題*/ 
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+
+if (redirect) {
+  const base = "/my-react-app";
+  window.history.replaceState(null, "", base + redirect);
+}
+
+/* */ 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
