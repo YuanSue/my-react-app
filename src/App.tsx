7315f4e85631d00f38e539f43fceb40be3ac96f4
YuanSue <guardianwings1996@gmail.com>
2026-01-26 10:44:15 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Link, useRoutes } from "react-router-dom";
+import { BrowserRouter, Link, useRoutes } from "react-router-dom";
 import jlptRoutes from "./routes/jlpt_rts";
 import "./App.css";
 
@@ -8,7 +8,7 @@ function AppRoutes() {
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter >
       <div className="container">
         <header className="header">
           <h1 className="title">元蘇學習網</h1>
@@ -23,7 +23,7 @@ function App() {
           <AppRoutes />
         </main>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
