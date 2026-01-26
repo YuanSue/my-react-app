0a079b1b2e41bbfbc06a6f110373be8a379185c0
YuanSue <guardianwings1996@gmail.com>
2026-01-26 10:40:37 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function AppRoutes() {
 
 function App() {
   return (
-    <HashRouter>
+    <HashRouter basename="/my-react-app">
       <div className="container">
         <header className="header">
           <h1 className="title">元蘇學習網</h1>
