b671b4ea984060199feb4e8f8376990b4acb58d5
YuanSue <guardianwings1996@gmail.com>
2026-01-26 11:02:20 +0800

    new article

--- src/routes/jlpt_rts.tsx
+++ src/routes/jlpt_rts.tsx
@@ -1,6 +1,7 @@
 import JLPT from "../pages/JLPT";
 import N4_251113 from "../pages/pages_jp/2025_11_13";
 import N4_251225 from "../pages/pages_jp/2025_12_25";
+import N3_260115 from "../pages/pages_jp/2026_01_15";
 
 const jlptRoutes = {
   path: "JLPT",
@@ -9,6 +10,7 @@ const jlptRoutes = {
     { index: true, element: <N4_251113 /> }, // /JLPT
     { path: "n4_1", element: <N4_251113 /> },
     { path: "n4_2", element: <N4_251225 /> },
+    { path: "n3_1", element: <N3_260115 /> }
   ],
 };
 
