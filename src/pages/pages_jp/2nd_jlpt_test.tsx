ba3446058565cb4dbe5600ae159ef7b4cf9f20ce
YuanSue <guardianwings1996@gmail.com>
2026-05-05 07:35:30 +0800

    update pages

--- src/pages/pages_jp/2nd_jlpt_test.tsx
+++ src/pages/pages_jp/2nd_jlpt_test.tsx
@@ -1,5 +1,13 @@
+import Furi from "../../component/func_furi";
+
 export default function N3_t02() {
   return (
-<p></p>
+    <div className="font">
+      <h3>第一回JLPT 試題review</h3>
+      <h5>詞彙問題</h5>
+      <ul className="no-list-style">
+        <li>来月、旅行に行くので、お金を <Furi kanji="節約" kana="せつやく" />する。</li>
+      </ul>
+</div>
       );
 }
