71667075adaaa3f804c672f49bb75b7ccb3918be
YuanSue <guardianwings1996@gmail.com>
2026-06-08 17:42:20 +0800

    update

--- src/pages/pages_jp/3rd_jlpt_test.tsx
+++ src/pages/pages_jp/3rd_jlpt_test.tsx
@@ -196,9 +196,212 @@ export default function N3_t03() {
 
         <h5>語意問題</h5>
 
-      </ul>
+        {/* Q1 */}
+        {questions.QQ1.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        {/* Q2 */}
+        {questions.QQ2.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        {/* Q3 */}
+        {questions.QQ3.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+
+        {/* Q4 */}
+        {questions.QQ4.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+
+        {/* Q5 */}
+        {questions.QQ5.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        {/* Q6 */}
+        {questions.QQ6.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        {/* Q7 */}
+        {questions.QQ7.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        {/* Q8 */}
+        {questions.QQ8.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        {/* Q9 */}
+        {questions.QQ9.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        {/* Q10 */}
+        {questions.QQ10.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        {/* Q11 */}
+        {questions.QQ11.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        {/* Q12 */}
+        {questions.QQ12.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        {/* Q13 */}
+        {questions.QQ13.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        {/* Q14 */}
+        {questions.QQ14.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        {/* Q15 */}
+        {questions.QQ15.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        <Toggle title="ans">
+        <RedMarkdown content={Reads.ans_15} />
+        </Toggle>
+
+        {/* Q16 */}
+        {questions.QQ16.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        <Toggle title="ans">
+        <RedMarkdown content={Reads.ans_16} />
+        </Toggle>
+
+        {/* Q17 */}
+        {questions.QQ17.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        <Toggle title="ans">
+        <RedMarkdown content={Reads.ans_17} />
+        </Toggle>
+
+        {/* Q18 */}
+        {questions.QQ18.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        <Toggle title="ans">
+        <RedMarkdown content={Reads.ans_18} />
+        </Toggle>
+
+        
+        <RedMarkdown content={Reads.read_1} />
+
+        {/* Q19 */}
+        {questions.QQ19.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        <Toggle title="ans">
+        <RedMarkdown content={Reads.ans_19} />
+        </Toggle>
+
+        {/* Q20 */}
+        {questions.QQ20.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        <Toggle title="ans">
+        <RedMarkdown content={Reads.ans_20} />
+        </Toggle>
+
+        {/* Q21 */}
+        {questions.QQ21.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        <Toggle title="ans">
+        <RedMarkdown content={Reads.ans_21} />
+        </Toggle>
+
+        {/* Q22 */}
+        {questions.QQ22.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        <Toggle title="ans">
+        <RedMarkdown content={Reads.ans_22} />
+        </Toggle>
+
+        {/* Q23 */}
+        {questions.QQ23.sentences.map((s, i) => (
+          <li key={i} className="no-list-style">
+            <RedMarkdown content={s} />
+          </li>
+        ))}
+
+        <Toggle title="ans">
+        <RedMarkdown content={Reads.ans_23} />
+        </Toggle>
       
-      <p>{Reads.read_2}</p>
+      
+      </ul>
+
+      {/* <p>{Reads.read_2}</p> */}
     
       </div>
       );
